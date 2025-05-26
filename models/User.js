53c43ec8621da1482f9f4bd8037664c9153c1156
Virall228 <vb917185@gmail.com>
2025-05-26 22:27:31 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -71,6 +71,63 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function(next) {
     if (this.isModified('wallet.address')) {
         // Here you could add wallet address validation for TON
+        const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  telegramId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  nickname: {
+    type: String,
+    default: ''
+  },
+  profilePicture: {
+    type: String,
+    default: ''
+  },
+  stats: {
+    wins: {
+      type: Number,
+      default: 0
+    },
+    losses: {
+      type: Number,
+      default: 0
+    },
+    tournamentParticipation: {
+      type: Number,
+      default: 0
+    }
+  },
+  achievements: [{
+    name: String,
+    description: String,
+    dateEarned: Date
+  }],
+  favoriteGames: [{
+    type: String
+  }],
+  rank: {
+    type: String,
+    default: 'Rookie'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  lastActive: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', userSchema); 
     }
     next();
 });
