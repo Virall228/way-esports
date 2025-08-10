fff4a5eae8a28341def276f7cdc38d08fd2d0dbc
Virall228 <vb917185@gmail.com>
2025-08-10 13:15:15 +0200

    Delete way-esports directory

--- way-esports/frontend/src/components/Mobile/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as MobileGestures } from './MobileGestures';
-export { default as OptimizedImage } from './OptimizedImage';
-export { default as PerformanceOptimizer } from './PerformanceOptimizer'; 
