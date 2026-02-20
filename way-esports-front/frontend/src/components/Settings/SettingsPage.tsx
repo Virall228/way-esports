1c148af9facb942e7f066911e909aed663ddcaed
Virall228 <vb917185@gmail.com>
2026-02-20 15:14:59 +0100

    Update SettingsPage.tsx

--- way-esports-front/frontend/src/components/Settings/SettingsPage.tsx
+++ way-esports-front/frontend/src/components/Settings/SettingsPage.tsx
@@ -332,13 +332,7 @@ const SettingsPage: React.FC = () => {
                         <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
                             <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5 8-5v10zm-8-7L4 6h16l-8 5z"/>
                         </svg>
-                        <a href="mailto:support@wayesports.com">support@wayesports.com</a>
-                    </ContactItem>
-                    <ContactItem>
-                        <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
-                            <path d="M20 15.5c-1.25 0-2.45-.2-3.57-.57a1.02 1.02 0 00-1.02.24l-2.2 2.2a15.045 15.045 0 01-6.59-6.59l2.2-2.21a.96.96 0 00.25-1A11.36 11.36 0 018.5 4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1 0 9.39 7.61 17 17 17 .55 0 1-.45 1-1v-3.5c0-.55-.45-1-1-1zM19 12h2a9 9 0 00-9-9v2c3.87 0 7 3.13 7 7z"/>
-                        </svg>
-                        <a href="tel:+1234567890">+1 (234) 567-890</a>
+                        <a href="mailto:wayesports.org@gmail.com">wayesports.org@gmail.com</a>
                     </ContactItem>
                     <ContactItem>
                         <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
