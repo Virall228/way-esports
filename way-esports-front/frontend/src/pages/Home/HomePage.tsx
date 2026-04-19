6f2c850d8c52003cb0c3b46732bf726d13545223
Virall228 <vb917185@gmail.com>
2026-04-19 21:03:39 +0200

    new

--- way-esports-front/frontend/src/pages/Home/HomePage.tsx
+++ way-esports-front/frontend/src/pages/Home/HomePage.tsx
@@ -90,7 +90,7 @@ const Logo = styled.div`
   width: 120px;
   height: 120px;
   background:
-    url('/images/way-esports-logo-2026.jpg?v=1') center/cover no-repeat,
+    url('/images/way-main-logo-metal.jpg?v=2') center/cover no-repeat,
     url('/images/way-esports-logo.png.jpg') center/cover no-repeat;
   border-radius: 50%;
   display: flex;
